7a2e23b38c349bcadcca25be03da5fed9c8e3f96
Axollyon <Axollyon@users.noreply.github.com>
2022-11-10 22:21:02 -0500

    async stink

--- scripts/spell-itemizer.js
+++ scripts/spell-itemizer.js
@@ -4,6 +4,18 @@ Hooks.on("dnd5e.itemUsageConsumption", (item, config, options, usage) => {
     const itemize = item.parent.getFlag(MODULE_ID, "itemize");
     if (!itemize) return true;
     else {
+        Itemize(item, config, options, usage).then();
         return false;
     }
 });
+
+async function Itemize(item, config, options, usage) {
+    // Commit pending data updates
+    const { actorUpdates, itemUpdates, resourceUpdates } = usage;
+    if ( !foundry.utils.isEmpty(itemUpdates) ) await item.update(itemUpdates);
+    if ( config.consumeQuantity && (item.system.quantity === 0) ) await item.delete();
+    if ( !foundry.utils.isEmpty(actorUpdates) ) await item.parent.update(actorUpdates);
+    if ( resourceUpdates.length ) await item.parent.updateEmbeddedDocuments("Item", resourceUpdates);
+
+    
+}
